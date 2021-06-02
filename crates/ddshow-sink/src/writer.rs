36cd24d7bd141c55f89f26df52f9a9c4286cf62d
Chase Wilson <me@chasewilson.dev>
2021-06-02 15:55:10 -0500

    Alignment stuffs

--- crates/ddshow-sink/src/writer.rs
+++ crates/ddshow-sink/src/writer.rs
@@ -1,4 +1,3 @@
-use byteorder::{LittleEndian, WriteBytesExt};
 use ddshow_types::Event;
 use rkyv::{
     ser::{serializers::AlignedSerializer, Serializer},
@@ -14,6 +13,7 @@ use timely::dataflow::operators::capture::event::{
 pub struct EventWriter<T, D, W> {
     stream: W,
     buffer: AlignedVec,
+    position: usize,
     __type: PhantomData<(T, D)>,
 }
 
@@ -23,6 +23,7 @@ impl<T, D, W> EventWriter<T, D, W> {
         EventWriter {
             stream,
             buffer: AlignedVec::with_capacity(512),
+            position: 0,
             __type: PhantomData,
         }
     }
@@ -34,35 +35,55 @@ where
     T: for<'a> Serialize<AlignedSerializer<&'a mut AlignedVec>>,
     D: for<'a> Serialize<AlignedSerializer<&'a mut AlignedVec>>,
 {
-    /// Push an event to the event writer
     fn push(&mut self, event: TimelyEvent<T, D>) {
         let event: Event<T, D> = event.into();
 
-        let archive_len = {
-            self.buffer.clear();
+        // Align to 16
+        const PADDING: [u8; 15] = [0; 15];
+        match self.position & 15 {
+            0 => (),
+            x => {
+                let padding = 16 - x;
+                self.position += padding;
 
-            let mut serializer = AlignedSerializer::new(&mut self.buffer);
-            serializer
-                .serialize_value(&event)
-                .unwrap_or_else(|unreachable| match unreachable {});
+                if let Err(err) = self.stream.write_all(&PADDING[..padding]) {
+                    #[cfg(feature = "tracing")]
+                    _tracing::error!(
+                        padding_len = padding,
+                        "failed to write padding to stream: {:?}",
+                        err,
+                    );
 
-            serializer.pos()
-        };
-
-        if let Err(err) = self.stream.write_u64::<LittleEndian>(archive_len as u64) {
-            #[cfg(feature = "tracing")]
-            _tracing::error!(
-                archive_len = archive_len,
-                "failed to write archive length to stream: {:?}",
-                err,
-            );
-
-            return;
+                    return;
+                }
+            }
         }
 
-        if let Err(err) = self.stream.write_all(&self.buffer[..archive_len]) {
+        // Write archive
+        self.buffer.clear();
+
+        let mut serializer = AlignedSerializer::new(&mut self.buffer);
+        serializer
+            .serialize_value(&event)
+            .unwrap_or_else(|unreachable| match unreachable {});
+
+        let archive_len = serializer.pos() as u128;
+
+        let result = self
+            .stream
+            // This will keep 16-byte alignment because archive_len is a u128
+            .write_all(&archive_len.to_le_bytes())
+            .and_then(|_| self.stream.write_all(&self.buffer));
+
+        self.position += 16 + self.buffer.len();
+
+        if let Err(err) = result {
             #[cfg(feature = "tracing")]
-            _tracing::error!("failed to write buffer data to stream: {:?}", err);
+            _tracing::error!(
+                archive_len = %archive_len,
+                "failed to write buffer data to stream: {:?}",
+                err,
+            );
 
             return;
         }
